3a7dd3a56568e1ec2fd2f30b2453a9053870c624
darecc <dariusz.ceglarek@gmail.com>
2021-07-25 17:46:14 +0200

    wyzerowanie projektu

--- src/dc/Main.java
+++ src/dc/Main.java
@@ -2,9 +2,6 @@ package dc;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Julia and Tomasz");
-    }
-    public static void display() {
         System.out.println("Hello everybody");
     }
 }
